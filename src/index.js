1232e523c9bf043daca6633a848db18401cef218
Daniel Joel <161968293+jinXNestroy@users.noreply.github.com>
2024-04-22 22:20:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,24 +11,24 @@ const rating = document.querySelector('#rating-display')
 const comment = document.querySelector('#comment-display')
 const form = document.querySelector('#new-ramen')
 // Callbacks
-
+// Adding submit event listener to the form
 const addSubmitListener = () => {
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     handleSubmit()
 
-    // Add code
+    
 
   })
 }
-
+// Fetch data and convert
 const displayRamens = () => {
   fetch(endpoint).then((res) => res.json())
     .then(displayAllRamens)
-  // Add code
+  
 };
 
-
+// Iterate through the data and retrive data to be displayed
 function displayAllRamens(ramens) {
   ramens.forEach(renderEachRamen)
 }
@@ -41,7 +41,7 @@ function renderEachRamen(ramen) {
   ramenImage.addEventListener('click', e => renderRamen(ramen))
 
 }
-
+// Individual Ramen Function
 function renderRamen(ramen) {
   imageRamen.src = ramen.image;
   individualName.textContent = ramen.name;
@@ -49,7 +49,7 @@ function renderRamen(ramen) {
   rating.textContent = ramen.rating
   comment.textContent = ramen.comment
 }
-
+// Display the submitted Ramen
 function renderNewRamen() {
   const ramenImage = document.createElement('img')
   const inputName = document.querySelector('#new-name')
@@ -65,6 +65,7 @@ function renderNewRamen() {
   imageRamen.src = inputImage.value
 }
 
+// Handle the submit event listener
 function handleSubmit() {
   const ramenImage = document.createElement('img')
   const inputName = document.querySelector('#new-name')
